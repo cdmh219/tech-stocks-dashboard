52e71a5953e8cc2293905ddcc298bb8a74bbd6ec
Chanel Henley <cdhenley219@gmail.com>
2025-02-20 19:06:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <select>
+      <option value=""></option>
+      <option value="a">This is a test A</option>
+      <option value="">This is a test B</option>
+    </select>
+    </>
+  )
+}
+
+export default App
